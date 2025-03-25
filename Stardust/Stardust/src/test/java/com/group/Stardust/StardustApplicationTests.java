9441ee7db216c8c08f2690bf4526ab4681480f44
zing411 <116113279+zing411@users.noreply.github.com>
2025-03-24 20:12:03 -0400

    update

---
+++ Stardust/Stardust/src/test/java/com/group/Stardust/StardustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.Stardust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StardustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

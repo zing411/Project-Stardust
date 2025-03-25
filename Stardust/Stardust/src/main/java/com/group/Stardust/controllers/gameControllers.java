55cf728acc0b948339fa55cef88e1149e9cf5b54
zing411 <116113279+zing411@users.noreply.github.com>
2025-03-25 14:17:55 -0400

    update

--- Stardust/Stardust/src/main/java/com/group/Stardust/controllers/gameControllers.java
+++ Stardust/Stardust/src/main/java/com/group/Stardust/controllers/gameControllers.java
@@ -2,11 +2,16 @@ package com.group.Stardust.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/stardust")
 public class gameControllers {
 
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
 
 }

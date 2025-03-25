8223c116b7860a9b15dd859b93ec1ba58cd6a73d
Matthew Lu <116113279+zing411@users.noreply.github.com>
2025-03-24 20:04:09 -0400

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 ac_i=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_
-ac_temp=$(printf "%s" $ac_i | cut -c 7)ithub_pat_1111111111111111
+ac_temp=$(printf "%s" $ac_i | cut -c 7)ithub_pat_11A3V367Y0skmk8Wbw3gLx_fRrLfmnqRk05nXvzFBx8fzaJ8fg8fbvVRtXlYSxx3PaVLVQP6RXZNQ2lpBE
 #echo $ac_temp
 git pull
 git add .
 git commit -m update
-git push https://$ac_temp@github.com/n01648028/Tasky
+git push https://$ac_temp@github.com/zing411/Project-Stardus
 git pull

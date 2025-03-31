73898a1b022056c4d60b05a33d053fdefc762d39
mohamedyusufahmed-sectionE <157053667+mohamedyusufahmed-sectionE@users.noreply.github.com>
2025-03-31 15:53:14 -0400

    update

--- Stardust/Stardust/src/main/java/com/group/Stardust/models/Games.java
+++ Stardust/Stardust/src/main/java/com/group/Stardust/models/Games.java
@@ -1,5 +1,16 @@
 package com.group.Stardust.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Games {
+    private String title;
+    private String genre;
+    private double price;
+    private double rating;
+
 }
